32b0bc29edbdc6fd42675a98b2c8af8f5c6b3462
gagask <kholkovskii@mail.ru>
2026-03-12 19:24:22 +0300

    Добавил ToolBar с иконками

--- app/src/main/java/com/example/vkeducation/MainActivity.kt
+++ app/src/main/java/com/example/vkeducation/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.vkeducation
 
+import android.graphics.drawable.PaintDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.vkeducation.ui.theme.VKeducationTheme
 
@@ -20,8 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             VKeducationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    RuStoreApps(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +39,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun RuStoreApps(modifier: Modifier = Modifier) {
+
+    Column {
+        RuStoreAppsToolbar()
+        LazyColumn {}
+    }
+}
+
+@Composable
+fun RuStoreAppsToolbar(){
+    Row(){
+        Icon(
+            painter = painterResource(R.drawable.store_24px),
+            contentDescription = "RuStore icon",
+        )
+        Text("RuStore")
+        Spacer(
+            modifier = Modifier.weight(1F)
+        )
+        Icon(
+            painter = painterResource(R.drawable.menu_24px),
+            contentDescription = null
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun RuStoreAppsPreview() {
     VKeducationTheme {
-        Greeting("Android")
+        RuStoreApps()
     }
 }
