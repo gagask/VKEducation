ae593dedf0ce51b8ae361090f9a234c8610a3fbf
gagask <kholkovskii@mail.ru>
2026-03-15 15:28:53 +0300

    Добавил ViewModel implementation

---
+++ app/src/main/java/com/example/vkeducation/app_details/AppDetailsViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.vkeducation.app_details
+
+import androidx.lifecycle.ViewModel
+
+class AppDetailsViewModel: ViewModel() {
+}
---
+++ app/src/main/java/com/example/vkeducation/app_list/AppListViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.vkeducation.app_list
+
+import androidx.lifecycle.ViewModel
+
+class AppListViewModel: ViewModel() {
+}
