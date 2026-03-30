2b45d1b727bea19498a33d1ced762beb92189d00
gagask <kholkovskii@mail.ru>
2026-03-30 18:37:43 +0300

    Для выполнения 3-го условия ДЗ

--- app/src/main/java/com/example/vkeducation/presentation/appdetails/AppDetailsVIewModel.kt
+++ app/src/main/java/com/example/vkeducation/presentation/appdetails/AppDetailsVIewModel.kt
@@ -54,21 +54,25 @@ class AppDetailsViewModel @Inject constructor(
         }
     }
 
+    suspend fun load() {
+        _state.value = AppDetailsState.Loading
+
+        runCatching {
+            val appDetails = repository.getAppDetails(id)
+
+            _state.value = AppDetailsState.Content(
+                appDetails = appDetails,
+                descriptionCollapsed = false,
+            )
+        }.onFailure { error ->
+            Log.d("AppDetailsVM", "${error.message}")
+            _state.value = AppDetailsState.Error
+        }
+    }
+
     fun getAppDetails() {
         viewModelScope.launch {
-            _state.value = AppDetailsState.Loading
-
-            runCatching {
-                val appDetails = repository.getAppDetails(id)
-
-                _state.value = AppDetailsState.Content(
-                    appDetails = appDetails,
-                    descriptionCollapsed = false,
-                )
-            }.onFailure { error ->
-                Log.d("AppDetailsVM", "${error.message}")
-                _state.value = AppDetailsState.Error
-            }
+            load()
         }
     }
 }
